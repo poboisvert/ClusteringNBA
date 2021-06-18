4e74568499011bfdef853a3ee4a08e95a1d272b1
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-06-18 15:20:39 -0400

    Import dataset and fetch all users

--- db/app/server/routes/nba.py
+++ db/app/server/routes/nba.py
@@ -4,7 +4,7 @@ from fastapi.encoders import jsonable_encoder
 from app.server.database import (
     add_dataset,
     retrieve_player,
-    retrieve_players,
+    retrieve_allplayers,
 )
 from app.server.models.nba import (
     ErrorResponseModel,
@@ -15,9 +15,22 @@ from app.server.models.nba import (
 router = APIRouter()
 
 
-@router.get("/add_dataset", response_description="Dataset data added into the database")
-async def add_stream_data():
+@router.post("/dataset", response_description="Dataset data added into the database")
+async def dataset(filename='datasets/finalized_data.csv'):
+
     # stream = jsonable_encoder(stream)
     # Call add_dataset
-    confirm = await add_dataset()
-    return {"message": "Dataset is alive"}
+    confirm = await add_dataset(filename)
+    return {confirm, "Dataset is alive"}
+
+
+@ router.get("/allplayers", response_description="All players retreived from the database")
+async def get_players_data():
+    allplayers = await retrieve_allplayers()
+    return ResponseModel(allplayers, "All players fetch successfully.")
+
+
+@ router.get("/player/{id}", response_description="All players retreived from the database")
+async def get_players_data(id):
+    player = await retrieve_player(id)
+    return ResponseModel(player, "Player fetch successfully.")
