2f4486c5d6d110993639aab10d0abb915a81aeb3
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-06-19 13:09:42 -0400

    ignore

--- db/app/server/database.py
+++ db/app/server/database.py
@@ -12,9 +12,10 @@ client = MongoClient(MONGO_DETAILS)
 # Database Name
 db = client.players
 
-# Collection Name
+# Collection Name - Datasets
 seasons_collection = db.get_collection("Season_Dataset")
 cleaned_collection = db.get_collection("Cleaned_Dataset")
+# Collection Name - Players
 players_collection = db.get_collection("Player_Dataset")
 
 
@@ -30,40 +31,40 @@ def player_helper(player) -> dict:
         # "GS": player["GS"],
         "MP": player["MP"],
         "PER": player["PER"],
-        "TS_rel": player["TS_rel"],
-        "treePAr": player["treePAr"],
-        "FTr": player["FTr"],
-        "ORB_rel": player["ORB_rel"],
-        "DRB_rel": player["DRB_rel"],
-        "TRB_rel": player["TRB_rel"],
-        "AST_rel": player["AST_rel"],
-        "STL_rel": player["STL_rel"],
-        "BLK_rel": player["BLK_rel"],
-        "TOV_rel": player["TOV_rel"],
-        "USG_rel": player["USG_rel"],
-        "blanl": player["blanl"],
-        "OWS": player["OWS"],
-        "DWS": player["DWS"],
-        "WS": player["WS"],
-        "WSon48": player["WSon48"],
+        # "TS_rel": player["TS_rel"],
+        # "treePAr": player["treePAr"],
+        # "FTr": player["FTr"],
+        # "ORB_rel": player["ORB_rel"],
+        # "DRB_rel": player["DRB_rel"],
+        # "TRB_rel": player["TRB_rel"],
+        # "AST_rel": player["AST_rel"],
+        # "STL_rel": player["STL_rel"],
+        # "BLK_rel": player["BLK_rel"],
+        # "TOV_rel": player["TOV_rel"],
+        # "USG_rel": player["USG_rel"],
+        # "blanl": player["blanl"],
+        # "OWS": player["OWS"],
+        # "DWS": player["DWS"],
+        # "WS": player["WS"],
+        # "WSon48": player["WSon48"],
         # "blank2": player["blank2"],
-        "OBPM": player["OBPM"],
-        "DBPM": player["DBPM"],
-        "BPM": player["BPM"],
-        "VORP": player["VORP"],
+        # "OBPM": player["OBPM"],
+        # "DBPM": player["DBPM"],
+        # "BPM": player["BPM"],
+        # "VORP": player["VORP"],
         "FG": player["FG"],
-        "FGA": player["FGA"],
-        "FG_rel": player["FG_rel"],
-        "treeP": player["treeP"],
-        "treePA": player["treePA"],
-        "treeP_rel": player["treeP_rel"],
-        "twoP": player["twoP"],
-        "twoPA": player["twoPA"],
-        "twoP_rel": player["twoP_rel"],
-        "eFG_rel": player["eFG_rel"],
+        # "FGA": player["FGA"],
+        # "FG_rel": player["FG_rel"],
+        # "treeP": player["treeP"],
+        # "treePA": player["treePA"],
+        # "treeP_rel": player["treeP_rel"],
+        # "twoP": player["twoP"],
+        # "twoPA": player["twoPA"],
+        # "twoP_rel": player["twoP_rel"],
+        # "eFG_rel": player["eFG_rel"],
         "FT": player["FT"],
-        "FTA": player["FTA"],
-        "FT_rel": player["FT_rel"],
+        # "FTA": player["FTA"],
+        # "FT_rel": player["FT_rel"],
         "ORB": player["ORB"],
         "DRB": player["DRB"],
         "TRB": player["TRB"],
@@ -77,7 +78,7 @@ def player_helper(player) -> dict:
         "year_start": player["year_start"],
         # "year_end": player["year_end"],
         # "position": player["position"],
-        "height": player["height"],
+        # "height": player["height"],
         "weight": player["weight"],
         # "birth_date": player["birth_date"],
         # "college": player["college"],
@@ -139,6 +140,7 @@ async def clean_dataset():
     # Drop
     b_df = pd.DataFrame(columns=clean_df.columns)
 
+    # Matching on Player and year_born players DataFrame
     for index, row in clean_df.iterrows():
         subset = clean_df[(clean_df["Year"] == row["Year"]) & (
             clean_df["Player"] == row["Player"]) & (clean_df["year_born"] == row["year_born"])]
@@ -147,6 +149,7 @@ async def clean_dataset():
         elif subset[subset["Tm"] == "TOT"].index[0] not in b_df.index:
             b_df = b_df.append(subset[subset["Tm"] == "TOT"])
 
+    # Converting height from feets to cm
     b_df["Status"] = (b_df["Year"] - b_df["year_start"] + 1)
 
     # Seperate feet and inches and convert all measurments to inches
@@ -160,10 +163,12 @@ async def clean_dataset():
     b_df = b_df.drop(columns=['Tm', 'height', 'feet', 'inch',
                               'blanl', 'blank2', 'FG_rel', 'treeP_rel', 'twoP_rel', 'FT_rel'], axis=1)
 
+    #   Business rule
     b_df = b_df[b_df["G"] > 10]
 
     # Export to csv
     b_df.to_csv('datasets/SeasonsDataCleaned.csv', index=False)
 
     # print(collection_pandas_df)
+    print('Max Pain 1.01 Completed - Cleaned')
     return b_df
